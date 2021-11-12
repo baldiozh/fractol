2ae4dac0789c16e2851b03209ee5d1d5a23f7688
baldiozh <baldiozh@icloud.com>
2021-11-12 19:12:46 +0300

    mandelbrot

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,7 +6,7 @@
 /*   By: gmckinle <gmckinle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 17:26:51 by gmckinle          #+#    #+#             */
-/*   Updated: 2021/11/12 19:02:36 by gmckinle         ###   ########.fr       */
+/*   Updated: 2021/11/12 19:07:59 by gmckinle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,11 +58,11 @@ void	mandelbrot(t_fractal *f)
 				f->z = ComplexInit(
 					pow(f->z.re, 2.0) - pow(f->z.im, 2.0) + f->c.re,
 					2.0 * f->z.re * f->z.im + f->c.im);
-				my_mlx_pixel_put(&f->image, f->x, f->y, 0X00FFF0F0);
 				iter++;
 			}
 			// Установка цвета точки
-			// my_mlx_pixel_put(&f->image, f->x, f->y, 0X00FFF0F0);
+			if (pow(f->z.re, 2.0) + pow(f->z.im, 2.0) > 4)
+				my_mlx_pixel_put(&f->image, f->x, f->y, 0X00FFF0F0);
 			f->x++;
 		}
 		f->y++;
