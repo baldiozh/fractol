5f0849d8aced58b43c4cb1f03fa3eacddd4a9344
baldiozh <baldiozh@icloud.com>
2021-11-12 19:14:49 +0300

    mandelbrot

--- mandelbrot.c
+++ mandelbrot.c
@@ -6,36 +6,11 @@
 /*   By: gmckinle <gmckinle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/07 17:26:51 by gmckinle          #+#    #+#             */
-/*   Updated: 2021/11/12 19:07:59 by gmckinle         ###   ########.fr       */
+/*   Updated: 2021/11/12 19:14:44 by gmckinle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
-
-void	testmoves(t_fractal *f)
-{
-	/* white window */
-	while(f->y < HEIGHT)
-	{
-		f->x = 0;
-		while(f->x < WIDTH)
-		{
-			my_mlx_pixel_put(&f->image, f->x, f->y, 0X00FFF0F0);
-			f->x++;
-		}
-		f->y++;
-	}
-	f->x = 0;
-	f->y = 0;
-	/* coordinate plane */
-	while (f->x < 800 && f->y < 800)
-	{
-		my_mlx_pixel_put(&f->image, f->x, 400, 0X000F0FFF);
-		my_mlx_pixel_put(&f->image, 400, f->y, 0X000F0FFF);
-		f->x++;
-		f->y++;
-	}
-}
 
 void	mandelbrot(t_fractal *f)
 {
