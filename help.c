9ac834ffb2c117eabe25c47a2ea97aad09a29904
Gideon Mckinley <baldiozh@icloud.com>
2021-12-07 21:57:35 +0300

    help upd

--- help.c
+++ help.c
@@ -6,7 +6,7 @@
 /*   By: gmckinle <gmckinle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/25 21:36:39 by gmckinle          #+#    #+#             */
-/*   Updated: 2021/12/07 21:49:51 by gmckinle         ###   ########.fr       */
+/*   Updated: 2021/12/07 21:57:00 by gmckinle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,13 @@
 
 void    help()
 {
-    ft_putstr_fd("\x1b[1;32m---------------------------------\n", 1);
-    ft_putstr_fd("\x1b[------- KEYS FOR USING PROGRAM -------\n", 1);
-    ft_putstr_fd("\x1b[32m--- Iterations     [-][+]       ---\n", 1);
-    ft_putstr_fd("\x1b[32m--- Color          [C]          ---\n", 1);
-    ft_putstr_fd("\x1b[32m--- Moves          [←][↑][↓][→] ---\n", 1);
-    ft_putstr_fd("\x1b[32m--- Exit           [esc]        ---\n", 1);
-    ft_putstr_fd("\x1b[32m----------------------------------\n", 1);
+    ft_putstr_fd("\x1b[1;32m------------------------------------\n", 1);
+    ft_putstr_fd("\x1b[32m-*-*-  KEYS FOR USING PROGRAM  -*-*-\n", 1);
+    ft_putstr_fd("\x1b[32m-*- Iterations     [-][+]        -*-\n", 1);
+    ft_putstr_fd("\x1b[32m-*- Color          [C]           -*-\n", 1);
+    ft_putstr_fd("\x1b[32m-*- Moves          [←][↑][↓][→]  -*-\n", 1);
+    ft_putstr_fd("\x1b[32m-*- Exit           [esc]         -*-\n", 1);
+    ft_putstr_fd("\x1b[32m------------------------------------\n", 1);
     
 }
 
