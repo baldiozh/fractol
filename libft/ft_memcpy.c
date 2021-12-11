83a4a5dd9ea9eab1e987278fe63f48ea56ead20c
Gideon Mckinley <baldiozh@icloud.com>
2021-12-11 21:21:02 +0300

    norm libft upd

--- libft/ft_memcpy.c
+++ libft/ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: gmckinle <gmckinle@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/29 17:27:30 by gmckinle          #+#    #+#             */
-/*   Updated: 2021/11/29 17:27:31 by gmckinle         ###   ########.fr       */
+/*   Updated: 2021/12/11 21:20:19 by gmckinle         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memcpy (void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*d;
